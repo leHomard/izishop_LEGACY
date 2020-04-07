f8937d5d993bd92ae4373e1b04826729ce7c3993
Omar HARRAS <omarharras@micro.home>
2020-04-08 00:28:06 +0200

    fix token generation with userId

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ path: ".env" });
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const createServer = require("./createServer");
 
@@ -7,6 +8,15 @@ const server = createServer();
 
 // Middleware to handle JWT cookies
 server.express.use(cookieParser());
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    console.log("token ; ", userId);
+  }
+  next();
+});
 
 server.start(
   {
