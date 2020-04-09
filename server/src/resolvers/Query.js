3a85a3f0b6ab6b4a9e49f81b783a21ece0307697
Omar HARRAS <omarharras@micro.home>
2020-04-09 22:33:20 +0200

    [server] add me query

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -7,6 +7,13 @@ const Query = {
     const item = await ctx.db.query.item({ where: { id } }, info);
     return item;
   },
+  me: async (parent, { userName }, context, info) => {
+    const user = await context.db.query.user({ where: { userName } });
+    if (!user) {
+      throw new Error("query me : user not found");
+    }
+    return user;
+  },
 };
 
 module.exports = Query;
