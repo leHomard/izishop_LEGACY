56a1c6c1ac9bd7f79a9520a474996cc4bf6dadd2
Omar HARRAS <omarharras@micro.home>
2020-04-13 16:41:26 +0200

    add drawer component

--- client/components/header/Header.js
+++ client/components/header/Header.js
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
 import Link from "next/link";
-import styled from "styled-components";
 
 import Nav from "../nav/Nav";
 import StyledHeader, {
@@ -9,15 +8,6 @@ import StyledHeader, {
   StyledNav,
   StyledSearch,
 } from "./styles";
-import SubNav from "../nav/SubNav";
-
-const SubHeader = styled.div`
-  width: 100%;
-  position: fixed;
-  top: 2;
-  left: 0;
-  margin-bottom: 1rem;
-`;
 
 const Header = () => {
   return (
